b4863cbb7a75e8e34370611200a621c020b5d320
lvndquist <jonatanlundqvistsilins@gmail.com>
2025-12-04 20:22:20 +0100

    add tests for PUT users/:id

--- backend/test/baseData.js
+++ backend/test/baseData.js
@@ -11,6 +11,7 @@ async function baseData() {
             mail: "admin@test.com",
             password: await bcrypt.hash(process.env.TEST_PASSWORD, 10),
             role: "admin",
+            phone: "123456789",
             verified: true,
             createdAt: new Date()
         },
@@ -18,6 +19,7 @@ async function baseData() {
             mail: "user@test.com",
             password: await bcrypt.hash(process.env.TEST_PASSWORD, 10),
             role: "customer",
+            phone: "012345678",
             verified: true,
             createdAt: new Date()
         }
