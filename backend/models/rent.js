8558534e7f5e40e037c26023a2b9dace9588eeb9
lvndquist <jonatanlundqvistsilins@gmail.com>
2025-12-07 01:42:39 +0100

    add /rent/stop route

--- backend/models/rent.js
+++ backend/models/rent.js
@@ -67,7 +67,7 @@ const users = {
                 return {
                     error: {
                         status: 404,
-                        path: `POST api/v1/rent/${bikeID}`,
+                        path: `POST api/v1/rent/start/${bikeID}`,
                         title: "Not found",
                         message: `Bike with ID: ${bikeID} could not be found`
                     }
@@ -78,7 +78,7 @@ const users = {
                 return {
                     error: {
                         status: 409,
-                        path: `POST api/v1/rent/${bikeID}`,
+                        path: `POST api/v1/rent/start/${bikeID}`,
                         title: "Conflict",
                         message: `Bike with ID: ${bikeID} is busy and cant be rented.`
                     }
@@ -94,10 +94,10 @@ const users = {
     },
 
     validateRide: async function(user, bikeID) {
-        let dbRides = await database.getDb("rides");
+        let dbRides
 
         try {
-
+            dbRides = await database.getDb("rides");
             const activeRide = await dbRides.collection.findOne({bike: new ObjectId(bikeID), status: "active"});
             
             // trying to rent an active bike
@@ -105,25 +105,12 @@ const users = {
                 return {
                     error: {
                         status: 409,
-                        path: `POST api/v1/rent/${bikeID}`,
+                        path: `POST api/v1/rent/start/${bikeID}`,
                         title: "Conflict",
                         message: `Bike with ID: ${bikeID} is busy and cant be rented.`
                     }
                 }               
             }
-            
-            // only user who started the ride or an admin can make changes
-            /*
-            if (!user.role !== "admin" && activeRide.user !== user.id) {
-                return {
-                    error: {
-                        status: 403,
-                        path: `POST api/v1/rent/${bikeID}`,
-                        title: "Forbidden",
-                        message: "You are not allowed to make changes to this ride"
-                    }
-                }        
-            } */
             
             return activeRide
         } finally {
@@ -134,10 +121,10 @@ const users = {
     },
 
     updateBike: async function(bikeID) {
-        let dbBikes = await database.getDb("bikes");
+        let dbBikes
         
         try {
-
+            dbBikes = await database.getDb("bikes");
             const updateBike = await dbBikes.collection.findOneAndUpdate(
                 { _id: new ObjectId(bikeID) },
                 { $set: { status: "rented" } },
@@ -148,7 +135,7 @@ const users = {
                 return {
                     error: {
                         status: 404,
-                        path: `POST api/v1/rent/rent/${bikeID}`,
+                        path: `POST api/v1/rent/start/${bikeID}`,
                         title: "Not found",
                         message: `Could not update bike's status. BikeID: ${bikeID}`
                     }
@@ -162,16 +149,16 @@ const users = {
     },
         
     validateUser: async function(userID, bikeID) {
-        let dbUser = await database.getDb("users");
+        let dbUser
         
         try {
-
+            dbUser = await database.getDb("users");
             const user = await dbUser.collection.findOne({_id: new ObjectId(userID)});
             if (!user) {
                 return {
                     error: {
                         status: 404,
-                        path: `POST api/v1/rent/rent/${bikeID}`,
+                        path: `POST api/v1/rent/start/${bikeID}`,
                         title: "Not found",
                         message: `Could not find User with ID: ${req.user.id}`
                     }
@@ -182,7 +169,7 @@ const users = {
                 return {
                     error: {
                         status: 402,
-                        path: `POST api/v1/rent/rent/${bikeID}`,
+                        path: `POST api/v1/rent/start/${bikeID}`,
                         title: "Insufficient balance",
                         message: "Users balance is too low to start a ride."
                     }
@@ -196,10 +183,10 @@ const users = {
     },
 
     createRide: async function(bikeID, userID, bikePos) {
-        let dbRides = await database.getDb("rides");
+        let dbRides
 
         try {
-
+            dbRides = await database.getDb("rides");
             const createRide = await dbRides.collection.insertOne({
                 user: new ObjectId(userID),
                 bike: new ObjectId(bikeID),
@@ -276,7 +263,7 @@ const users = {
 
             return res.status(201).json({
                  data: {
-                    message: "Bike has been rented",
+                    message: "Ride has started",
                     bikeID: bikeID,
                     userID: req.user.id,
                     rideID: validateRes
@@ -286,12 +273,167 @@ const users = {
             return res.status(500).json({
                 errors: {
                     status: 500,
-                    path: `POST api/v1/rent/${bikeID}`,
+                    path: `POST api/v1/rent/start/${bikeID}`,
                     title: "Database error",
                     message: e.message
                 }
             });
         } 
+    },
+
+    validateActiveRide: async function (user, bikeID) {
+        let dbRides
+
+        try {
+            dbRides = await database.getDb("rides");
+            const activeRide = await dbRides.collection.findOne({
+                bike: new ObjectId(bikeID),
+                active: true
+            });
+            
+            if (!activeRide) {
+                return {
+                    error: {
+                        status: 404,
+                        path: `POST api/v1/rent/stop/${bikeID}`,
+                        title: "Not found",
+                        message: `Could not find ride for bike with id: ${bikeID}`
+                    }
+                }               
+            }
+
+            // only user who started the ride or an admin can make changes
+            if (!user.role !== "admin" && activeRide.user !== user.id) {
+                return {
+                    error: {
+                        status: 403,
+                        path: `POST api/v1/rent/${bikeID}`,
+                        title: "Forbidden",
+                        message: "You are not allowed to stop this ride"
+                    }
+                }        
+            }
+            
+            return activeRide;
+
+        } finally {
+            await dbRides.client.close();
+        }        
+    },
+
+    updateBikeAvailable: async function (bikeID) {
+        let dbBikes
+
+        try {
+            dbBikes = await database.getDb("bikes");
+            const updateBike = await dbBikes.collection.findOneAndUpdate(
+                { _id: new ObjectId(bikeID) },
+                { $set: { status: "free" } },
+                { returnDocument: "after" }
+            );
+            
+            if (!updateBike) {
+                return {
+                    error: {
+                        status: 404,
+                        path: `POST api/v1/rent/stop/${bikeID}`,
+                        title: "Not found",
+                        message: `Could not find bike with id: ${bikeID}`
+                    }
+                }                
+            }
+            
+            return updateBike;
+
+        } finally {
+            await dbBikes.client.close();
+        }        
+    },
+
+    calculateCost: function(start, stop) {
+        const time = stop - start;
+        const minutes = Math.ceil(time/ (1000 * 60))
+        const price = helpers.startingFee + helpers.minuteFee * minutes;
+        return { minutes, price }
+    },
+
+    endRide: async function (bikeID, rideID, userID) {
+        let dbRides
+        let dbUsers
+
+        try {
+            dbRides = await database.getDb("rides");
+            const ride = await dbRides.collection.findOne({ _id: new ObjectId(rideID)});
+            
+            if (!ride) {
+                return {
+                    error: {
+                        status: 404,
+                        path: `POST api/v1/rent/stop/${bikeID}`,
+                        title: "Not found",
+                        message: `Could not find ride with id: ${rideID}`
+                    }
+                }               
+            }
+            
+            const stop = new Date();
+            const { duration, price } = this.calculateCost(ride.start, stop);
+            
+            const update = await dbRides.collection.findOneAndUpdate(
+                { _id: new ObjectId(rideID) },
+                {
+                    $set: {
+                        stop: stop,
+                        stopPos: bikePos,
+                        duration: duration,
+                        price: price,
+                        parking: parkingType,
+                        active: false
+                    }
+                },
+                { returnDocument: "after" }
+            );
+
+            if (!update) {
+                return {
+                    error: {
+                        status: 500,
+                        path: `POST api/v1/rent/stop/${bikeID}`,
+                        title: "Could not update",
+                        message: "Could not end ride"
+                    }
+                }
+            }
+
+            dbUsers = await database.getDb("users");
+            const user = await dbUsers.collection.findOneAndUpdate(
+                { _id: new ObjectId(userID) },
+                { $inc: { balance: -price }},
+                { returnDocument: "after" }
+            );
+
+            if(!user) {
+                return {
+                    error: {
+                        status: 500,
+                        path: `POST api/v1/rent/stop/${bikeID}`,
+                        title: "Could not update",
+                        message: "Could not update user balance"
+                    }
+                }                   
+            }
+
+            return {
+                ride: update,
+                duration: duration,
+                price: price,
+                balance: user.balance
+            }
+
+        } finally {
+            await dbRides.client.close();
+            await dbUsers.client.close();
+        }        
     },
 
     stopRide: async function (res, req) {
@@ -308,144 +450,53 @@ const users = {
             });
         }
 
-    },
-
-    /**
-     * An admin can list all users.
-     */
-    changeStatus: async function (res, req) {
-        const changeTo = req.body.changeTo;
-        const bikeID = req.body.bikeID;
-
-        if (!bikeID) {
-            return res.status(400).json({
-                error: {
-                    status: 400,
-                    path: `POST api/v1/rent/${bikeID}`,
-                    title: "Bad Request",
-                    message: "Bike ID is missing"
-                }
-            });
-        }
-
-        if (!changeTo) {
-            return res.status(400).json({
-                error: {
-                    status: 400,
-                    path: `POST api/v1/rent/${bikeID}`,
-                    title: "Bad Request",
-                    message: "changeTo field is missing"
-                }
-            });
-        }
-
-        if (!helpers.changeToIsValid(changeTo)) {
-            return res.status(400).json({
-                error: {
-                    status: 400,
-                    path: `POST api/v1/rent/${bikeID}`,
-                    title: "Bad Request",
-                    message: "changeTo field is not a valid option"
-                }
-            });
-        }
-
-        let dbBikes;
-        let dbRides;
-
         try {
-            dbBikes = await database.getDb("bikes");
-            dbRides = await database.getDb("rides");
-            
-            const bike = await dbBikes.collection.findOne({_id: new ObjectId(bikeID)});
-            
-            if (!bike) {
-                return res.status(404).json({
-                    error: {
-                        status: 404,
-                        path: `POST api/v1/rent/${bikeID}`,
-                        title: "Not found",
-                        message: `Bike with ID: ${bikeID} could not be found`
-                    }
-                });
+            let validateRes;
+
+            validateRes = await this.validateBike(bikeID);
+            if (validateRes.error) {
+                return res.status(validateRes.error.status).json(validateRes.error);
             }
 
-            // trying to rent an active bike
-            if (bike.status === "rented" && changeTo === "rented") {
-                return res.status(409).json({
-                    error: {
-                        status: 409,
-                        path: `POST api/v1/rent/${bikeID}`,
-                        title: "Conflict",
-                        message: `Bike with ID: ${bikeID} is busy and cant be rented.`
-                    }
-                });                
+            validateRes = await this.validateActiveRide(req.user, bikeID)
+            if (validateRes.error) {
+                return res.status(validateRes.error.status).json(validateRes.error);
             }
+            const activeRideID = validateRes._id
 
-            const ride = await dbRides.collection.findOne({bike: new ObjectId(bikeID), status: "active"});
-
-            // only user who started the ride or an admin can make changes
-            if (!req.user.role !== "admin" && ride.user !== req.user.id) {
-                return res.status(403).json({
-                    error: {
-                        status: 403,
-                        path: `POST api/v1/rent/${bikeID}`,
-                        title: "Forbidden",
-                        message: "You are not allowed to make changes to this ride"
-                    }
-                });                   
+            validateRes = await this.updateBikeAvailable(bikeID)
+            if (validateRes.error) {
+                return res.status(validateRes.error.status).json(validateRes.error);
             }
+            const bikePos = validateRes.position;
 
-            // trying to rent an active bike
-            if (ride && changeTo === "rented") {
-                return res.status(409).json({
-                    error: {
-                        status: 409,
-                        path: `POST api/v1/rent/${bikeID}`,
-                        title: "Conflict",
-                        message: `Bike with ID: ${bikeID} is busy and cant be rented.`
-                    }
-                });                
-            }
-
-            const update = await dbBikes.collection.findOneAndUpdate(
-                { _id: new ObjectId(bikeID) },
-                { $set: { status: changeTo } },
-                { returnDocument: "after" }
-            );
-
-            if (!update) {
-                return res.status(500).json({
-                    error: {
-                        status: 500,
-                        path: `POST api/v1/rent/${bikeID}`,
-                        title: "Could not verify user",
-                        message: "There was a problem verifying the user."
-                    }
-                });                
+            validateRes = await this.endRide(bikeID, activeRideID, req.user.id, bikePos)
+            if (validateRes.error) {
+                return res.status(validateRes.error.status).json(validateRes.error);
             }
 
             return res.status(200).json({
                  data: {
-                    message: "Bike status updated",
-                    bike: {
-                        status: changeTo,
-                        id: bike._id
-                    }
+                    message: "Ride has ended",
+                    bikeID: bikeID,
+                    userID: req.user.id,
+                    rideID: validateRes.ride._id,
+                    duration: validateRes.duration,
+                    price: validateRes.price,
+                    balance: validateRes.balance
                  }
             });
         } catch (e) {
             return res.status(500).json({
                 errors: {
                     status: 500,
-                    path: `POST api/v1/rent/${bikeID}`,
+                    path: `POST api/v1/rent/start/${bikeID}`,
                     title: "Database error",
                     message: e.message
                 }
             });
-        } finally {
-            await db.client.close();
-        }
+        } 
+
     },
 }
 
