fb3c5b9fbb1f3ce9bc7e067f55ffec9fa4b2cfa7
lvndquist <jonatanlundqvistsilins@gmail.com>
2026-01-05 02:36:31 +0100

    refactor initDB

--- backend/routes/v1/payment.js
+++ backend/routes/v1/payment.js
@@ -4,6 +4,12 @@ var router = express.Router();
 const auth = require("../../models/auth.js");
 const payment = require("../../models/payment.js");
 
+/***************************************************************************************************
+ | Uri                                       |  GET  | POST | PUT | PATCH | DELETE |
+ |-------------------------------------------|:-----:|:----:|:---:|:-----:|:------:|
+ | /v1/payment/{userid}/fill                 |  -    | Yes  | -   |  -    |  -     |
+ ***************************************************************************************************/
+
 // POST api/v1/payment/:userID/fill
 // Fill balance
 router.post('/:userID/fill',
--- backend/utils/initDB.js
+++ backend/utils/initDB.js
@@ -2,45 +2,26 @@
 
 const database = require('../database/database');
 const bcrypt = require('bcryptjs');
+const standardData = require("./standardData.json");
 
-module.exports = async function initDB() {
-    console.log("Preparing DB")
-    const { collection: collectionUsers, client } = await database.getDb("users");
-    collectionUsers.deleteMany({})
-    const users = [
-        {
-            mail: "admin@test.com",
-            role: "admin",
-            phone: "123456789",
-            balance: 20000,
-            verified: true,
-        },
-        {
-            mail: "user@test.com",
-            role: "customer",
-            phone: "012345678",
-            verified: true,
-            balance: 200,
-        },
-        {
-            mail: "user1@test.com",
-            role: "customer",
-            phone: "01234567832",
-            verified: true,
-            balance: 0,
-        },
-        {
-            mail: "user2@test.com",
-            role: "customer",
-            phone: "0134567832",
-            verified: true,
-            balance: 200,
-        }
-    ];
+async function createUsers() {
 
-    for (const user of users) {
+    console.log("Adding users to DB")
+
+    let db = await database.getDb("users");
+
+    const minLength = standardData.users.length;
+    const currentLength = await db.collection.countDocuments({});
+    if (currentLength >= minLength) {
+        console.log("Users already in DB. Skipping.");
+        return;
+    }
+
+    await db.collection.deleteMany({});
+
+    for (const user of standardData.users) {
         const password = await bcrypt.hash(process.env.TEST_PASSWORD, 10)
-        await collectionUsers.updateOne(
+        await db.collection.updateOne(
             { mail: user.mail },
             {
                 $setOnInsert: {
@@ -52,119 +33,335 @@ module.exports = async function initDB() {
             { upsert: true }
         );
     }
+
+    db.client.close()
+
     console.log("Users added to DB")
+}
 
-    const { collection: collectionBikes } = await database.getDb("bikes");
+async function createCities() {
 
-    const bikes = [
-        {
-            city: "",
-            currentZone: "",
-            currentStation: "",
-            number: 1,
-            battery: 100,
-            status: "free",
-            position: {}
-        },
-        {
-            city: "",
-            currentZone: "",
-            currentStation: "",
-            number: 2,
-            battery: 100,
-            status: "rented",
-            position: {}
-        },
-        {
-            city: "",
-            currentZone: "",
-            currentStation: "",
-            number: 3,
-            battery: 100,
-            status: "free",
-            position: {}
-        }
-    ];
+    console.log("Adding cities to DB")
 
-    for (const bike of bikes) {
-        await collectionBikes.updateOne(
-            { number: bike.number },
-            {
-                $setOnInsert: { ...bike }
-            },
-            { upsert: true }
-        );
+    let db = await database.getDb("cities");
+
+    const minLength = standardData.cities.length;
+    const currentLength = await db.collection.countDocuments({});
+    if (currentLength >= minLength) {
+        console.log("Cities already in DB. Skipping.");
+        return;
     }
 
-    console.log("Bikes added to DB")
-    
-    const { collection: collectionCities } = await database.getDb("cities");
-    
-    const cities = [
-        {
-            name: "",
-            zones: [],
-            stations: [],
-        }
-    ]
 
-    for (const city of cities) {
-        await collectionCities.updateOne(
+    await db.collection.deleteMany({});
+    
+    for (const city of standardData.cities) {
+        await db.collection.updateOne(
             { name: city.name },
             {
-                $setOnInsert: { ...city }
+                $setOnInsert: { ...city, createdAt: new Date() }
+            },
+            { upsert: true }
+        );
+    }
+    db.client.close()
+    console.log("Cities added to DB")
+}
+
+async function createZones() {
+    
+    console.log("Adding zones to DB")
+
+    let dbZones = await database.getDb("zones");
+    let dbCities = await database.getDb("cities");
+
+    const minLength = standardData.zones.length;
+    const currentLength = await dbZones.collection.countDocuments({});
+    if (currentLength >= minLength) {
+        console.log("Zones already in DB. Skipping.");
+        return;
+    }
+
+
+    await dbZones.collection.deleteMany({});   
+
+    const cities = await dbCities.collection.find().toArray();
+
+    for (const zone of standardData.zones) {
+        const cityObject = cities.find(city => city.name === zone.cityID)        
+        await dbZones.collection.updateOne(
+            { name: zone.name },
+            {
+                $setOnInsert: {
+                     ...zone,
+                     cityID: cityObject._id,
+                     createdAt: new Date()
+                }
+            },
+            { upsert: true }
+        );
+    }
+    
+    console.log("Zones added to DB")
+    console.log("Adding zones references to 'city' collection")
+
+    const zones = await dbZones.collection.find().toArray();
+    for (const city of cities) {
+        cityZones = zones.filter(zone => zone.cityID.toString() === city._id.toString())
+        zoneIDs = cityZones.map(zone => zone._id)
+
+        if (zoneIDs.length > 0) {
+            await dbCities.collection.findOneAndUpdate(
+                {
+                    _id: city._id
+                },
+                {
+                    $push: { zones: { $each: zoneIDs }}
+                }
+            );
+        }
+    }
+
+    dbCities.client.close()
+    dbZones.client.close()
+
+    console.log("Zones references added to 'city' collection");
+
+}
+
+async function createStations() {
+    
+    console.log("Adding stations to DB")
+
+    let dbStations = await database.getDb("stations");
+    let dbCities = await database.getDb("cities");
+    let dbZones = await database.getDb("zones");
+
+    const minLength = standardData.stations.length;
+    const currentLength = await dbStations.collection.countDocuments({});
+    if (currentLength >= minLength) {
+        console.log("Stations already in DB. Skipping.");
+        return;
+    }
+
+    await dbStations.collection.deleteMany({});   
+
+    const cities = await dbCities.collection.find().toArray();
+    const zones = await dbZones.collection.find().toArray();
+
+    for (const station of standardData.stations) {
+        const cityObject = cities.find(city => city.name === station.cityID)        
+        const zoneObject = zones.find(zone => zone.name === station.zoneID)        
+        
+        await dbStations.collection.updateOne(
+            { name: station.name },
+            {
+                $setOnInsert: {
+                     ...station,
+                     cityID: cityObject._id,
+                     zoneID: zoneObject._id,
+                     createdAt: new Date()
+                }
+            },
+            { upsert: true }
+        );
+    }
+    
+    console.log("Stations added to DB")
+    console.log("Adding station references to 'city' collection")
+
+    const stations = await dbStations.collection.find().toArray();
+    for (const city of cities) {
+        cityStations = stations.filter(station => station.cityID.toString() === city._id.toString())
+        stationIDs = cityStations.map(station => station._id)
+
+        if (stationIDs.length > 0) {
+            await dbCities.collection.findOneAndUpdate(
+                {
+                    _id: city._id
+                },
+                {
+                    $push: { stations: { $each: stationIDs }}
+                }
+            );
+        }
+    }
+
+    dbCities.client.close()
+    dbZones.client.close()
+    dbStations.client.close()
+
+    console.log("Station references added to 'city' collection");
+}
+
+async function createBikes() {
+    console.log("Adding bikes to DB")
+    
+    let dbBikes = await database.getDb("bikes");
+    let dbCities = await database.getDb("cities");
+    let dbZones = await database.getDb("zones");
+    let dbStations = await database.getDb("stations");
+
+    const minLength = standardData.bikes.length;
+    const currentLength = await dbBikes.collection.countDocuments({});
+    if (currentLength >= minLength) {
+        console.log("Bikes already in DB. Skipping.");
+        return;
+    }
+
+    await dbBikes.collection.deleteMany({});
+
+    const cities = await dbCities.collection.find().toArray();
+    const zones = await dbZones.collection.find().toArray();
+    const stations = await dbStations.collection.find().toArray();
+
+    // map each collections entry name to id/object
+    const cityMap = new Map(cities.map(city => [city.name, city._id]))
+    const zoneMap = new Map(zones.map(zone => [zone.name, zone._id]))
+    const stationMap = new Map(stations.map(station => [station.name, station]))
+
+    for (const bike of standardData.bikes) {
+        
+        const cityID = cityMap.get(bike.city);
+        if (!cityID) {
+            console.warn(`Unknown city: ${bike.city}. Skipping bike nr: ${bike.number}.`)
+            continue;
+        }
+
+        const zoneID = bike.currentZoneName ? zoneMap.get(bike.currentZoneName) : null;
+        const stationID = bike.currentStationName ? stationMap.get(bike.currentStationName)._id: null;
+        const stationPos = bike.currentStationName ? stationMap.get(bike.currentStationName).position: null;
+        
+        await dbBikes.collection.updateOne(
+            { number: bike.number },
+            {
+                $setOnInsert: {
+                     ...bike, 
+                     cityID: cityID,
+                     zoneID: zoneID,
+                     stationID: stationID,
+                     position: stationPos,
+                     createdAt: new Date() 
+                }
             },
             { upsert: true }
         );
     }
 
-    console.log("Cities added to DB")
+    dbBikes.client.close()
+    dbCities.client.close()
+    dbZones.client.close()
+    dbStations.client.close()
+    
+    console.log("Bikes added to DB")
+}
 
-    const { collection: collectionZones } = await database.getDb("zones");
-    await collectionZones.deleteMany({});
+async function createPayments() {
 
-    const zones = [
-        {
-            name: "",
-            zones: [],
-            area: {},
-        }
-    ];
+    console.log("Adding dummy payments to DB")
 
-    for (const zone of zones) {
-        await collectionZones.updateOne(
-            { name: zone.name },
+    let dbPayments = await database.getDb("payments");
+    let dbUsers = await database.getDb("users");
+    
+    const minLength = standardData.payments.length;
+    const currentLength = await dbPayments.collection.countDocuments({});
+    if (currentLength >= minLength) {
+        console.log("Payments already in DB. Skipping.");
+        return;
+    }
+
+    await dbPayments.collection.deleteMany({});
+    
+    const users = await dbUsers.collection.find().toArray();
+
+    for (const payment of standardData.payments) {
+        findUser = users.find(user => user.mail === payment.user)
+        
+        await dbPayments.collection.insert(
             {
-                $setOnInsert: { ...zone }
+                user: findUser._id,
+                price: payment.price,
+                type: payment.type,
+                status: payment.status,
+                createdAt: new Date()
             },
-            { upsert: true }
         );
-    }        
+    }
 
-    console.log("Zones added to DB")
+    dbPayments.client.close();
+    dbUsers.client.close();
+    
+    console.log("Dummy payments added to DB")
+}
 
-    const { collection: collectionStations } = await database.getDb("stations");
-    await collectionStations.deleteMany({});
+async function createRides() {
 
-    const stations = [
-        {
-            name: "",
-            zones: [],
-            position: {},
-        }
-    ]
+    console.log("Adding dummy rides to DB")
 
-    for (const station of stations) {
-        await collectionZones.updateOne(
-            { name: station.name },
+    let dbRides = await database.getDb("rides");
+    let dbBikes = await database.getDb("bikes");
+    let dbUsers = await database.getDb("users");
+    
+    const minLength = standardData.rides.length;
+    const currentLength = await dbRides.collection.countDocuments({});
+    if (currentLength >= minLength) {
+        console.log("Rides already in DB. Skipping.");
+        return;
+    }
+
+    await dbPayments.collection.deleteMany({});
+
+    const users = await dbUsers.collection.find().toArray();
+    const bike1 = await dbBikes.collection.findOne({number: 1});
+    const bike2 = await dbBikes.collection.findOne({number: 2});
+    
+    const user = users[0]
+    const startPos = bike1.position;
+    const stopPos = bike2.position;
+    const id = bike1._id;
+    const start = new Date()
+    const stop = new Date(Date.now() + 1000 * 60 * 5)
+
+    for (const ride of standardData.rides) {
+        
+        await dbRides.collection.insert(
             {
-                $setOnInsert: { ...station }
+                user: user,
+                bike: id,
+                start: start,
+                stop: stop,
+                startPos: startPos,
+                stopPos: stopPos,
+                duration: Math.ceil((stop - start) / (1000 * 60)),
+                price: 100,
+                parking: ride.parking,
+                active: ride.active,
+                createdAt: new Date()
             },
-            { upsert: true }
         );
-    }    
-    console.log("Stations added to DB")
+    }
 
-    await client.close();
+    dbRides.client.close()
+    dbBikes.client.close()
+    dbUsers.client.close()
 
+    console.log("Dummy rides added to DB")
+}
+
+async function initDB() {
+    try {
+        console.log("Preparing DB")
+        await createUsers();
+        await createCities();
+        await createZones();
+        await createStations();
+        await createBikes();
+        await createPayments();
+        await createRides();
+        console.log("DB ready")
+    } catch (e) {
+        return new Error(e);
+    }
 };
+
+module.exports = {initDB}
